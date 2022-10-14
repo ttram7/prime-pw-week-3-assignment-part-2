4eb033e18edfc94f7c0042b14a72d041e7f257b1
Tina Tram <tinatram07@gmail.com>
2022-10-14 08:19:08 -0500

    created for loop that logs nums 0 to 5, 3 to 5, even numbers from 2 to 10

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -13,14 +13,26 @@ for (let i=0; i<4; i++) {
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
+for (let i=0; i<6; i++) {  
+  console.log(i);
+}
+//change the stopping condition
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
+for (let i=3; i<6; i++) {  
+  console.log(i);
+}
+//change the starting point
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
+for (let i=2; i<12; i+=2) {  
+  console.log(i);
+}
+//change starting point, stopping point, and how much i increases
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
