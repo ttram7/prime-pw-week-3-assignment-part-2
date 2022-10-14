eb7731295ce485ac446a221a20dceed678fa379c
Tina Tram <tinatram07@gmail.com>
2022-10-14 08:37:17 -0500

    created for loop that counts down from 5 to 0, for loop that logged each star in array, while loop that logged each star in array, while loop that counts from 0 to 5, while loop that counts down from 10 to 5

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -36,7 +36,9 @@ for (let i=2; i<12; i+=2) {
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-
+for (let i=5; i >= 0; i--) {  
+  console.log(i);
+}
 
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
@@ -44,16 +46,33 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
-
+for (let i=0; i<stars.length; i++) {  
+  console.log(stars[i]);
+}
 
 // 3. While loops  
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
+let index = 0;
+while (index < stars.length) {
+  console.log(stars[index]);
+  index++;
+}
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
+let i = 0;
+while (i < 6) {
+  console.log(i)
+  i++
+}
 
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+let max = 10;
+while (max > 4) {
+  console.log(max);
+  max--;
+}
